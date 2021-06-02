142f035027c60fbb6ad26d1e9866eb9cef3a634d
Rin0059 <fandorin0059@gmail.com>
2021-06-02 10:17:26 +0000

    [Finish]3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,12 +4,14 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @new_book = Book.new
     @user = @book.user
+    @book_comment = BookComment.new
   end
 
   def index
     @books = Book.all
     @book = Book.new
     @user = current_user
+    @book_comment = BookComment.new
   end
 
   def create
